e3af0fa280c3f2dfd281fd6d58f1f16e6e9f288a
yuesh <805924865@qq.com>
2016-05-25 14:59:17 +0800

    remove show.php from github
    remove show.php from github

--- show.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-use Novel\Panlong as Panlong;
-use Novel\Fanren as Fanren;
-use Novel\Xingchen as Xingchen;
-
-echo Panlong\Panlong2016::info();
-echo Fanren\Fanren2016::info();
-echo Xingchen\Xingchen2016::info();
